3a662cda8fcad885977ddcdcc33bb071c182bab5
stefansnoek <stefan.snoek123@gmail.com>
2025-03-23 18:20:50 +0100

    update frontend

--- frontend/src/pages/UserProfilePage.js
+++ frontend/src/pages/UserProfilePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import UserProfile from '../components/UserProfile';
 
 const UserProfilePage = () => {
-    return <UserProfile user={{ name: "Stefan Snoek", email: "john@example.com" }} />;
+    return <UserProfile user={{ name: "John Doe", email: "john@example.com" }} />;
 };
 
 export default UserProfilePage;
